624f0c99680e2b2598aae173cea2d85485c3cb9f
stephanie-ai <stephanie_cooper@hotmail.com>
2020-12-03 15:12:36 +0000

    create form with input for artist name and submit button

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+// constructor(props) {
+    //     super(props)
+    //     this.state = {
+    //         artists: []
+    //     }
+    // };
+    
+    state = {
+        artistName: ""
+    };
+
+    componentDidMount(){
+        console.log('Did mount')
+    };
+
+    componentDidUpdate(){
+        console.log('Did update')
+    };
+    
+    // searchArtist(artistName) {
+    //     ArtistSearchAPI.searchArtist(artistName).then((artists) => {
+    //         this.setState({ artists: artists })
+    //     }
+    // };
+
     render() {
         return (
-            <h1>Render other button here</h1>
+            <>
+                <h1>Render button here</h1>
+                <div>
+                    <form onSubmit={this.handleFormSubmit}>
+                        <input type="text" name="name" placeholder="Search for an Artist" onChange={this.handleInputChange} />
+                        <input type="submit" id="submit" value="Search" />
+                    </form>
+                </div>
+            </>
         )
     }
 }
